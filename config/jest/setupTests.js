ecc60a8feea850531a16439f2c3a974e8de78302
Joe Ng <joengsh@gmail.com>
2023-02-13 13:42:57 +0000

    feat: config jest

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable no-undef */
-import '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/jest-dom/extend-expect';
 
-import { enableFetchMocks } from 'jest-fetch-mock';
-import React from 'react';
+// import { enableFetchMocks } from 'jest-fetch-mock';
+// import React from 'react';
 
-global.React = React; // this also works for other globally available libraries
-enableFetchMocks();
+// global.React = React; // this also works for other globally available libraries
+// enableFetchMocks();
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 // /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   // preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   // coverageProvider: 'v8',
